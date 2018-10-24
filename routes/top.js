c51b4118315326012978f2941fce4eb1e0dbf284
tsu2626 <dmwtpdmwtp24@gmail.com>
2018-10-24 23:59:35 +0900

    fix state

--- routes/top.js
+++ routes/top.js
@@ -2,52 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 let https = require('https');
-// Bitcoin のレートを json で取得することができるAPI
-const BTC = "https://coincheck.com/api/rate/btc_jpy";
-
-https.get(BTC, function (res) {
-    var body = '';
-    res.setEncoding('utf8');
-    res.on('data', function (chunk) {
-        body += chunk;
-    });
-    res.on('data', function (chunk) {
-        // body の値を json としてパースしている
-        res = JSON.parse(body);
-      });
-});
-
-const ETH = "https://coincheck.com/api/rate/eth_jpy";
-
-https.get(ETH, function (res) {
-  var body = '';
-  res.setEncoding('utf8');
-  res.on('data', function (chunk) {
-      body += chunk;
-  });
-  res.on('data', function (chunk) {
-      // body の値を json としてパースしている
-      res = JSON.parse(body);
-    });
-});
 
 const XRP = "https://coincheck.com/api/rate/xrp_jpy";
 
-https.get(XRP, function (res) {
-  var body = '';
-  res.setEncoding('utf8');
-  res.on('data', function (chunk) {
-      body += chunk;
-  });
-  res.on('data', function (chunk) {
-      // body の値を json としてパースしている
-      res = JSON.parse(body);
-  });
+var state = {};
+
+https.get(XRP, (res) => {
+    var body = '';
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+        body += chunk;
+    });
+    res.on('end', (chunk) => {
+      state = JSON.parse(body);
+      console.log(`現在のレートは${state.rate}円/XRP`);
+    })
+  }).on('error', (e) => {
+    console.log(e.message);
 });
+
 router.get('/', (req, res, next) => {
   res.render('top', {
     title: 'TOP',
-    rate: `${res.rate}`
+    rate: `現在のレートは${state.rate}円/XRP`
   });
 });
 
